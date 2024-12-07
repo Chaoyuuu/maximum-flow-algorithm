54b6b92bd6cad5247ddc7dce0cd2741f58fbdb35
楊育承 <yangyucheng@youikushous-MacBook-Pro.local>
2024-12-06 17:04:48 -0800

    adjust compare function

--- compare_algorithms.py
+++ compare_algorithms.py
@@ -1,9 +1,8 @@
 import os
 import time
-import tracemalloc
 from preflow_push_maximum_flow import read_graph_from_file
 from ford_fulkerson import ford_fulkerson, read_file
-from scaling_ford_fulkerson import max_flow_func
+from scaling_ford_fulkerson import read_graph, build_capacity_matrix, get_source_and_sink, scaling_ford_fulkerson
 
 DIR_BASE_PATH = 'GraphGenerator/'
 
@@ -15,17 +14,9 @@ def get_all_file_names(folder_path):
                   if os.path.isfile(os.path.join(folder_path, f)) and f.endswith('.txt')]
     return file_names
 
-def measure_memory_and_time(algorithm, file_path):
-    tracemalloc.start()
-    start_time = time.time()
-    
-    max_flow = algorithm(file_path)
-    
-    end_time = time.time()
-    current, peak = tracemalloc.get_traced_memory()
-    tracemalloc.stop()
-    
-    return max_flow, end_time - start_time, peak
+def execute_algorithm_and_measure_time(algorithm, file_path):
+    max_flow, elapsed_time = algorithm(file_path)
+    return max_flow, elapsed_time
 
 def ford_fulkerson_alg(file_path):
     #read graph
@@ -39,20 +30,33 @@ def ford_fulkerson_alg(file_path):
     )
     
     #call ford_fulkerson algorithm
+    start_time = time.time()
     max_flow = ford_fulkerson(graph, source, sink)
-    return max_flow
+    end_time = time.time()
+    
+    return max_flow, end_time - start_time
 
 def scaling_ford_fulkerson_alg(file_path):
-    max_flow = max_flow_func(file_path)
-    return max_flow
+    
+    nodes, edges, node_to_index = read_graph(file_path)
+    capacity, _ = build_capacity_matrix(nodes, edges)
+    source, sink = get_source_and_sink(node_to_index)
+    
+    start_time = time.time()
+    max_flow = scaling_ford_fulkerson(capacity, source, sink)
+    end_time = time.time()
+    
+    return max_flow, end_time - start_time
 
 def preflow_push_alg(file_path):
     graph = read_graph_from_file(file_path)
 
     source = 0
     sink = 1
+    start_time = time.time()
     max_flow = graph.preflow_push(source, sink)
-    return max_flow
+    end_time = time.time()
+    return max_flow, end_time - start_time
 
 # Function to compare three different algorithms
 def compare_algorithms():
@@ -63,10 +67,10 @@ def compare_algorithms():
     random_graph = get_all_file_names("./GraphGenerator/Random")
     
     graph_types = [
-        ("Bipartite", bipartite_graph),
-        ("FixedDegree", fixed_degree_graph),
+        # ("Bipartite", bipartite_graph),
+        # ("FixedDegree", fixed_degree_graph),
         ("Mesh", mesh_graph),
-        ("Random", random_graph)
+        # ("Random", random_graph)
     ]
     
     algorithms = [
@@ -86,11 +90,10 @@ def compare_algorithms():
             for algo_name, algorithm in algorithms:
                 file_path = DIR_BASE_PATH + graph_type + '/' + filename
                 
-                max_flow, elapsed_time, peak_mem = measure_memory_and_time(algorithm, file_path)
+                max_flow, elapsed_time = execute_algorithm_and_measure_time(algorithm, file_path)
             
                 print(f"  •{algo_name} - Max Flow: {max_flow}")
                 print(f"    ◦ Time: {elapsed_time:.4f} seconds")
-                print(f"    ◦ Peak Memory: {peak_mem / 1024:.2f} KB")
             print('- - - - - - - - - - - - - - - - - - - - - - -')
 
 
